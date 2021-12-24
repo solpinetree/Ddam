a50459fee188dde4ab50150b4e09d8ee9f78daef
Eden <haerim2429@gmail.com>
2021-12-24 18:15:10 +0900

    index

--- src/main/java/com/ddam/spring/controller/IndexController.java
+++ src/main/java/com/ddam/spring/controller/IndexController.java
@@ -14,4 +14,42 @@ public class IndexController {
 	public String index() {
 		return "index";
 	}
+<<<<<<< Updated upstream
 }
+=======
+	
+	@GetMapping("/list")
+	public String list() {
+		return "list";
+	}
+	
+	@GetMapping("/recruitCrew")
+	public String recruitCrew() {
+		return "recruitCrew";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/newForm")
+	public String newForm() {
+		return "newForm";
+	}
+	
+	@GetMapping("/community")
+	public String community() {
+		return "community";
+	}
+	
+	@GetMapping("/createCrew")
+	public String createCrew() {
+		return "createCrew";
+	}
+	
+	
+	
+	
+}
+>>>>>>> Stashed changes
