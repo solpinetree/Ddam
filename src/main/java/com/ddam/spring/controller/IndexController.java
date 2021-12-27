823f126858a3b4929be1c763e69e6b74be4d022a
Eden <haerim2429@gmail.com>
2021-12-27 21:18:17 +0900

    index#3

--- src/main/java/com/ddam/spring/controller/IndexController.java
+++ src/main/java/com/ddam/spring/controller/IndexController.java
@@ -1,13 +1,25 @@
 package com.ddam.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ddam.spring.domain.Crew;
+import com.ddam.spring.repository.CrewRepository;
+import com.ddam.spring.service.CrewService;
 
 
 
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private CrewService crewService;
 
+	@Autowired
+	private CrewRepository crewRepository;
+	
 	
 	/*
 	 *  메인 페이지
@@ -15,6 +27,12 @@ public class IndexController {
 	@GetMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/crewdetail")
+	public String crewdetail(Long id, Model model) {
+		Crew crew = crewRepository.findById(id).orElse(null);
+		return "crew/crewdetail";
 	}
 	
 }
